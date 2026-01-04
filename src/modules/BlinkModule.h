fb1a94cb6813b19365443d5287b89bdde24bba1c
Dane <dane@goneepic.com>
2026-01-04 14:19:13 +1100

    Redo the LEDs to a more maintainable state.
    (cherry picked from commit c4686ed70133c7f7c62be2628dc5d3eb20de7880)

--- src/modules/BlinkModule.h
+++ src/modules/BlinkModule.h
@@ -9,35 +9,94 @@
 #include <functional>
 
 #ifndef FLAMINGO_BLINKY_IO
-#define BLINK_PIN 17  
+#define BLINK_PIN 17
 #else
 #define BLINK_PIN FLAMINGO_BLINKY_IO
-#endif 
+#endif
 
+#ifdef FLAMINGO_RT_LED
+// Range Test LED pins
+#ifndef PIN_LED_RT_R
+#define PIN_LED_RT_R (3)
+#endif
+#ifndef PIN_LED_RT_G
+#define PIN_LED_RT_G (2)
+#endif
+#ifndef PIN_LED_RT_B
+#define PIN_LED_RT_B (6)
+#endif
+#endif
+
+#ifdef FLAMINGO_CONNECTION_LED
+// Connection LED pins
+#ifndef PIN_LED_CONN_R
+#define PIN_LED_CONN_R (5)
+#endif
+#ifndef PIN_LED_CONN_G
+#define PIN_LED_CONN_G (4)
+#endif
+#ifndef PIN_LED_CONN_B
+#define PIN_LED_CONN_B (7)
+#endif
+#endif
 
 #define BLINK_ON HIGH
 #define BLINK_OFF LOW
 
-
+// LED Color enum
+enum class LEDColor { Off = 0, Red, Amber, Green, Purple, Blue, Teal, White };
 
 class BlinkModule : private concurrency::OSThread
 {
     bool firstTime = 1;
-    unsigned long blinkDurationMSecs = 1000;    // how long one blink should last
-    unsigned long blinkPauseMSecs = 2000;       // pause between blinks
-    unsigned long blinkStarted = 0;   //blink started
-    unsigned long blinkFinish = 0;   //finish time for blink
+    unsigned long blinkDurationMSecs = 1000; // how long one blink should last
+    unsigned long blinkPauseMSecs = 2000;    // pause between blinks
+    unsigned long blinkStarted = 0;          // blink started
+    unsigned long blinkFinish = 0;           // finish time for blink
     unsigned long pauseFinish = 0;
-    uint16_t currentBlink = 0;     // either 0 or non-zero
-    uint16_t blinkNumber = 0;       // number of blinks
+    uint16_t currentBlink = 0; // either 0 or non-zero
+    uint16_t blinkNumber = 0;  // number of blinks
+
+#if defined(FLAMINGO_RT_LED) || defined(FLAMINGO_CONNECTION_LED)
+                              // Helper method to set RGB LED color (using Red, Green, and optionally Blue pins)
+    // If pinB is 0, blue is treated as unavailable
+    void setRGBLEDColor(uint8_t pinR, uint8_t pinG, uint8_t pinB, LEDColor color);
+#endif
+
+#ifdef FLAMINGO_RT_LED
+    // Range Test LED state
+    bool rtLedsInitialized = false;
+    bool rtLedsActive = false;
+    unsigned long rtLedControlStartTime = 0;
+    static constexpr unsigned long RT_LED_TIMEOUT_MS = 7500; // 7.5 seconds
+#endif
+
+#ifdef FLAMINGO_CONNECTION_LED
+    // Connection LED state
+    bool connLedsInitialized = false;
+    bool connLedsActive = false;
+    unsigned long connLedControlStartTime = 0;
+    static constexpr unsigned long CONN_LED_TIMEOUT_MS = 60000; // 60 seconds (1 minute)
+    LEDColor connLedColor = LEDColor::Off;
+#endif
 
   public:
     BlinkModule();
 
+#ifdef FLAMINGO_RT_LED
+    // Range Test LED control
+    void setRangeTestLED(LEDColor color);
+    void setRangeTestLEDTimeout(unsigned long timeoutMs = RT_LED_TIMEOUT_MS);
+#endif
+
+#ifdef FLAMINGO_CONNECTION_LED
+    // Connection LED control
+    void setConnectionLED(LEDColor color);
+    void setConnectionLEDTimeout(unsigned long timeoutMs = CONN_LED_TIMEOUT_MS);
+#endif
+
   protected:
     virtual int32_t runOnce() override;
-    
-    
 };
 
 extern BlinkModule *blinkModule;
